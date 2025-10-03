cf2e8427c6a22f7d1b4e216685990a187632f698
TrieuHuy <trieuxuanhuytq3105@gmail.com>
2025-10-03 15:09:07 +0700

    clean

--- src/main/java/vn/hoidanit/jobhunter/JobhunterApplication.java
+++ src/main/java/vn/hoidanit/jobhunter/JobhunterApplication.java
@@ -2,15 +2,15 @@ package vn.hoidanit.jobhunter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.http.HttpMethod;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer.FrameOptionsConfig;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
-import org.springframework.security.web.header.writers.ReferrerPolicyHeaderWriter;
-import org.springframework.security.web.servlet.util.matcher.MvcRequestMatcher;
+// import org.springframework.context.annotation.Bean;
+// import org.springframework.http.HttpMethod;
+// import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+// import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer.FrameOptionsConfig;
+// import org.springframework.security.config.http.SessionCreationPolicy;
+// import org.springframework.security.web.SecurityFilterChain;
+// import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+// import org.springframework.security.web.header.writers.ReferrerPolicyHeaderWriter;
+// import org.springframework.security.web.servlet.util.matcher.MvcRequestMatcher;
 
 //disable security
 // @SpringBootApplication(exclude = {
--- src/main/java/vn/hoidanit/jobhunter/domain/Company.java
+++ src/main/java/vn/hoidanit/jobhunter/domain/Company.java
@@ -2,7 +2,7 @@ package vn.hoidanit.jobhunter.domain;
 
 import java.time.Instant;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
+// import com.fasterxml.jackson.annotation.JsonFormat;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
