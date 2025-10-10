ef85d8fdb74612dfc33add8ce345d06f513890d7
TrieuHuy <trieuxuanhuytq3105@gmail.com>
2025-10-10 17:06:08 +0700

    send email job

--- src/main/java/vn/huy/jobhunter/JobhunterApplication.java
+++ src/main/java/vn/huy/jobhunter/JobhunterApplication.java
@@ -11,6 +11,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 // import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 // import org.springframework.security.web.header.writers.ReferrerPolicyHeaderWriter;
 // import org.springframework.security.web.servlet.util.matcher.MvcRequestMatcher;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 //disable security
 // @SpringBootApplication(exclude = {
@@ -19,6 +20,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 // })
 
 @SpringBootApplication
+@EnableAsync
 public class JobhunterApplication {
 
 	public static void main(String[] args) {
--- src/main/java/vn/huy/jobhunter/repository/JobRepository.java
+++ src/main/java/vn/huy/jobhunter/repository/JobRepository.java
@@ -5,8 +5,12 @@ import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 import vn.huy.jobhunter.domain.Job;
+import java.util.List;
+import vn.huy.jobhunter.domain.Skill;
 
 @Repository
 public interface JobRepository extends JpaRepository<Job, Long>, JpaSpecificationExecutor<Job> {
     Boolean existsById(long id);
+
+    List<Job> findBySkillsIn(List<Skill> skills);
 }
